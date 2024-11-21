e5f9371420ea851ab0011a4b5acea984a2d9af28
pmkrawczyk <p.m.krawczyk@gmail.com>
2024-11-21 19:57:48 +0100

    Update top links and URLs for improved navigation; add new templates for uploads and workflows monitoring

--- omero_boost/urls.py
+++ omero_boost/urls.py
@@ -5,17 +5,26 @@ from . import views
 
 urlpatterns = [
     # Import page URLs
-    path('server_side_browser/', views.server_side_browser_page, name='server_side_browser_page'),
-    path('api/list_dir/', views.list_directory, name='list_directory'),
-    path('api/file_info/', views.file_info, name='file_info'),
-    path('api/import_selected/', views.import_selected, name='import_selected'),
-
+    path("server_side_browser/", views.omero_boost_upload, name="omero_boost_upload"),
+    path("api/list_dir/", views.list_directory, name="list_directory"),
+    path("api/file_info/", views.file_info, name="file_info"),
+    path("api/import_selected/", views.import_selected, name="import_selected"),
     # Database page URLs
-    path('imports/', views.imports_database_page, name='imports_database_page'),
-    path('workflows/', views.workflows_database_page, name='workflows_database_page'),
-
+    path(
+        "imports/",
+        views.omero_boost_monitor_uploads,
+        name="omero_boost_monitor_uploads",
+    ),
+    path(
+        "workflows/",
+        views.omero_boost_monitor_workflows,
+        name="omero_boost_monitor_workflows",
+    ),
     # Webclient templates and script menu
-    re_path(r'^webclient_templates/(?P<base_template>[a-z0-9_]+)/',
-            views.webclient_templates, name='webclient_templates'),
-    re_path(r'^get_script_menu/$', views.get_script_menu, name='get_script_menu'),
+    re_path(
+        r"^webclient_templates/(?P<base_template>[a-z0-9_]+)/",
+        views.webclient_templates,
+        name="webclient_templates",
+    ),
+    re_path(r"^get_script_menu/$", views.get_script_menu, name="get_script_menu"),
 ]
