e96cc66fde5b62fbea6bc47380af13fcc5e60714
Luik <t.t.luik@amsterdamumc.nl>
2025-01-29 11:32:20 +0100

    Up biomero to read from ~/slurm-config.ini

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
     install_requires=[
         "omero-web>=5.6.0", 
         "pyjwt",
-        "biomero>=2.0.0-alpha.1"],
+        "biomero>=2.0.0-alpha.2"],
     python_requires=">=3",
     include_package_data=True,
     zip_safe=False,
