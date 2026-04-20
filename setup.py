bbe69314e469a291555886181261ebf321518b64
Luik <t.t.luik@amsterdamumc.nl>
2026-04-20 13:56:36 +0200

    move to main branch

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(
     install_requires=[
         "omero-web>=5.6.0",
         "pyjwt",
-        "biomero @ git+https://github.com/NL-BioImaging/biomero.git@dev-zarr-export",
+        "biomero @ git+https://github.com/NL-BioImaging/biomero.git@main",
         "configupdater>=3.2",
         "biomero-importer>=1.0.0",
     ],
